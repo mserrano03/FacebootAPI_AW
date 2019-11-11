abca54f5d922bb59e84b75740d9dd60b30062288
Marco Serrano <marco030897@hotmail.com>
2019-11-10 23:12:39 -0700

    10-11-19
    Penultima version...

--- utils/logger.js
+++ utils/logger.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
-//const Log = require("log");
-//const fileLog = new Log("info", fs.createWriteStream("info.log", { flags: "a" }));
+const Log = require("log");
+const fileLog = new Log("info", fs.createWriteStream("info.log", { flags: "a" }));
 
 exports.info = function (message) {
     console.log(message);
-    //fileLog.info(message);
+    fileLog.info(message);
 }
 
 exports.error = function (message) {
     console.error(message);
-    //fileLog.error(message);
+    fileLog.error(message);
 }
